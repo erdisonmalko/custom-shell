65484e5eb87d46677c7f5b64e1cfaa665c6cca0d
erdisonmalko <erdisonmalko@gmail.com>
2026-02-01 22:09:50 +0100

    Adding history command

--- main.go
+++ main.go
@@ -62,28 +62,101 @@ func build_prefix(reader *bufio.Reader) (string, error) {
     return hostname, nil
 }
 
+func store_history(input string) error {
+    // Open the history file in append mode, create it if it doesn't exist
+    file, err := os.OpenFile(".simple_shell_history", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+    
+    counter := 0
+    // Count existing lines in the history file
+    data, err := os.ReadFile(".simple_shell_history")
+    if err == nil {
+        counter = strings.Count(string(data), "\n")
+    }
+    
+    // Write the input to the file with a newline and counter
+    if _, err := file.WriteString(fmt.Sprintf("%d %s\n", counter+1, input)); err != nil {
+        return err
+    }
 
+    return nil
+}
+
+func show_history() error {
+    data, err := os.ReadFile(".simple_shell_history")
+    if err != nil {
+        return err
+    }
+    fmt.Print(string(data))
+    return nil
+}
+
+
+func get_most_possible_command(input string) (string, error) {
+    
+    data, err := os.ReadFile(".simple_shell_history")
+    if err != nil {
+        return "", err
+    }
+    lines := strings.Split(string(data), "\n")
+    var possible_commands []string
+
+    for _, line := range lines {
+        if strings.HasPrefix(line, input) {
+            // Extract the command part after the line number
+            parts := strings.SplitN(line, " ", 2)
+            if len(parts) == 2 {
+                possible_commands = append(possible_commands, parts[1])
+            }
+        }
+    }
+
+
+    if len(possible_commands) == 0 {
+        return "", nil
+    }
+
+    // Return the first matched command as the most possible command
+    return possible_commands[0], nil
+}
 
 func executeInput(input string) error {
     input = strings.TrimSuffix(input, "\n")
     args := strings.Split(input, " ")
+
+    //store command in history
+    if err := store_history(input); err != nil {
+        return err
+    }
+
     switch args[0] {
         //in case of empty input, just return
-        case "":
+        // !, @, #, $, %, ^, &, *, (, ), _, +, {, }, |, :, ", <, >, ?, ~, \
+        case "", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", "|", ":", "\"", "<", ">", "?", "~", "\\":
+        return nil
+        case strings.Contains(input, "\t"):
+        fmt.Println("tab completion functionality is not implemented yet.")
         return nil
         case "history":
-        fmt.Println("history command is not implemented yet.")
+        if err := show_history(); err != nil {
+            return err
+        }
         return nil
-        //will work on tab completion later
-        case "tab":
-        fmt.Println("tab completion is not implemented yet.")
+        //will work on tab completion later 
+        case "9 0011 0x09":
+        if possible_cmd, err := get_most_possible_command(input); err != nil {
+            return err
+        } else {
+            fmt.Println(possible_cmd)
+        }
         return nil
         // will work on arrow keys later, up and down
-        case "arrow":
+        case "\x1b[A", "\x1b[B":
         fmt.Println("arrow key functionality is not implemented yet.")
         return nil
-        // SHELL ➜  exec: "\t": executable file not found in $PATH
-        //this errors happen as the input is not sanitized properly
         //exit command to exit the shell
         case "exit":
         os.Exit(0)
